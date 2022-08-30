2186d8d38d9dae87822205dca52ce17fe9945cc0
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-08-30 17:41:30 +0900

    removed unused css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,10 @@
 import { ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
-import { useState } from "react";
 import Layout from "../components/Layout";
-import { Board } from "../lib/types";
 import StoreProvider from "../stores/StoreProvider";
-import "../styles/globals.css";
 import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
     <StoreProvider>
       <ThemeProvider theme={theme}>
