dff4a5803633d5a9a2bc85c71e4d1b09638d3964
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-05 19:28:14 +0900

    added api url to env and added columns to getstaticprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,38 +4,50 @@ import type { GetStaticProps, NextPage } from "next";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import ColumnsList from "../components/Columns/ColumnsList";
-import { Board } from "../lib/types";
-import { setCurrentBoard } from "../stores/boardsSlice";
+import { Board, Column } from "../lib/types";
+import { setCurrentBoard, setCurrentBoardColumn } from "../stores/boardsSlice";
 
 type Props = {
   boards: Board[];
+  columns: Column[];
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await axios.get("http://localhost:8080/api/v1/boards");
-  const boards = response.data;
+  const boardsResponse = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/boards`);
+  const boards = boardsResponse.data;
+
+  const columnsResponse = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/boards/${boards[0].id}/columns`
+  );
+  const columns = columnsResponse.data;
 
   return {
     props: {
       boards,
+      columns,
     },
   };
 };
 
-const Home: NextPage<Props> = ({ boards }) => {
+const Home: NextPage<Props> = ({ boards, columns }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (window) {
       const currentBoardInLocalStorage =
         window.localStorage.getItem("currentBoard");
+      const columnsInLocalStorage = window.localStorage.getItem("columns");
 
-      if (currentBoardInLocalStorage) {
+      if (currentBoardInLocalStorage && columnsInLocalStorage) {
         const parsedCurrentBoard = JSON.parse(currentBoardInLocalStorage);
+        const parsedColumns = JSON.parse(columnsInLocalStorage);
         dispatch(setCurrentBoard(parsedCurrentBoard));
+        dispatch(setCurrentBoardColumn(parsedColumns));
       } else {
         dispatch(setCurrentBoard(boards[0]));
+        dispatch(setCurrentBoardColumn(columns));
         window.localStorage.setItem("currentBoard", JSON.stringify(boards[0]));
+        window.localStorage.setItem("columns", JSON.stringify(columns));
       }
 
       return () => {};
--- stores/apiSlice.ts
+++ stores/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/api/v1" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
   tagTypes: ["Boards", "Columns", "Tasks", "Subtasks"],
   endpoints: (builder) => ({}),
 });
