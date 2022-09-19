0d057ace6480e8932dbcdd0c6ce0071b309a89b3
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-19 17:07:12 +0900

    disable AddTaskButton if no columns

--- components/Tasks/AddTaskButton.tsx
+++ components/Tasks/AddTaskButton.tsx
@@ -1,7 +1,12 @@
 import AddIcon from "@mui/icons-material/Add";
 import { Button, Typography } from "@mui/material";
+import { skipToken } from "@reduxjs/toolkit/dist/query";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useBoardColumnsQuery } from "../../stores/api/columnsApi";
+import { RootState } from "../../stores/store";
 import AddEditTaskModal from "./AddEditTaskModal";
+import { MEDIUM_GREY_COLOR } from "../../styles/theme";
 
 const AddTaskButton = () => {
   const [open, setOpen] = useState(false);
@@ -14,29 +19,41 @@ const AddTaskButton = () => {
     setOpen(false);
   };
 
+  const currentBoard = useSelector(
+    (state: RootState) => state.boards.currentBoard
+  );
+
+  const { data: columns } = useBoardColumnsQuery(currentBoard || skipToken);
+
   return (
     <>
       <AddEditTaskModal open={open} onClose={handleClose} />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleOpen}
-        startIcon={<AddIcon />}
-        sx={{
-          ".MuiButton-startIcon": {
-            marginLeft: { xs: 0, md: -0.5 },
-            marginRight: { xs: 0, md: 0.5 },
-          },
-        }}
-      >
-        <Typography
-          variant="body1"
-          fontWeight="inherit"
-          sx={{ display: { xs: "none", md: "block" } }}
+      {columns && (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleOpen}
+          disabled={!columns.length}
+          startIcon={<AddIcon />}
+          sx={{
+            ".MuiButton-startIcon": {
+              marginLeft: { xs: 0, md: -0.5 },
+              marginRight: { xs: 0, md: 0.5 },
+            },
+            "&:disabled": {
+              backgroundColor: MEDIUM_GREY_COLOR,
+            },
+          }}
         >
-          Add new task
-        </Typography>
-      </Button>
+          <Typography
+            variant="body1"
+            fontWeight="inherit"
+            sx={{ display: { xs: "none", md: "block" } }}
+          >
+            Add new task
+          </Typography>
+        </Button>
+      )}
     </>
   );
 };
