af62dbb56430547fa100b910baaa6b81d17d68a8
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-05 10:42:35 +0900

    add prepareHeaders

--- stores/apiSlice.ts
+++ stores/apiSlice.ts
@@ -1,7 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { RootState } from './store';
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).user.accessToken;
+
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   tagTypes: ["Boards", "Columns", "Tasks", "Subtasks"],
   endpoints: (builder) => ({}),
 });
