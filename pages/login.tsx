20bca15d137bf7b319a7ed5cfe5f763ebdea4b19
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-05 10:45:06 +0900

    add login and signup pages

---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import Auth from "../components/Auth";
+
+type Props = {};
+
+const Login = (props: Props) => {
+  return <Auth action="login"/>;
+};
+
+export default Login;
---
+++ pages/signup.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Auth from '../components/Auth'
+
+type Props = {}
+
+const Signup = (props: Props) => {
+  return (
+    <Auth action="signup"/>
+  )
+}
+
+export default Signup
