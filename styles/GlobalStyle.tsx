88ab1e7bdd4979fd693640b5bfe47c22eb9a5ee0
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-05 10:42:49 +0900

    reset CSS

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -2,11 +2,23 @@ import { createGlobalStyle } from "styled-components";
 import { ScThemeType } from "./theme";
 
 const GlobalStyle = createGlobalStyle<{ theme: ScThemeType }>`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Plus Jakarta Sans", sans-serif
+  }
+
   html, body {
     min-height: 100vh;
     max-width: 100vw;
     overflow-x: auto;
     background-color: ${({ theme }) => theme.colors.background.dark};
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
   }
 `;
 
