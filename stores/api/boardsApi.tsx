f95b1fcc0bc24357ba5a47063607cd226222dd7f
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-06 11:00:12 +0900

    add toast to boards operations

--- stores/api/boardsApi.tsx
+++ stores/api/boardsApi.tsx
@@ -1,5 +1,7 @@
 import { Board } from "../../lib/types";
 import { apiSlice } from "../apiSlice";
+import { toast } from 'react-toastify';
+import { TOAST_OPTIONS } from "../../constants";
 
 export interface BoardPayload {
   id?: number;
@@ -16,11 +18,12 @@ const boardsApi = apiSlice.injectEndpoints({
       query: () => "/boards",
       providesTags: ["Boards", "Columns", "Subtasks", "Subtasks"],
     }),
+
     createBoard: builder.mutation<Board, BoardPayload>({
       query: (payload: BoardPayload) => {
         const columns = payload.columns
           .filter((column) => column.name !== "")
-          .map(({name}) => ({ name }));
+          .map(({ name }) => ({ name }));
 
         return {
           url: "/boards",
@@ -28,25 +31,51 @@ const boardsApi = apiSlice.injectEndpoints({
           body: { ...payload, columns },
         };
       },
+      transformResponse: async (response: Board | Promise<Board>, meta, args) => {
+        const created = meta?.response?.status === 201;
+        const { name } = await response
+
+        if (created) {
+          toast.success(`Created board '${name}'`, TOAST_OPTIONS);
+        }
+
+        return response
+      },
       invalidatesTags: ["Boards"],
     }),
+
     updateBoard: builder.mutation({
       query: (payload: BoardPayload) => {
-        const { id } = payload
+        const { id } = payload;
 
         return {
           url: `/boards/${id}`,
           method: "PATCH",
-          body: payload ,
+          body: payload,
         };
       },
-      invalidatesTags: ["Boards", "Columns"]
+      transformResponse: (response: Board | Promise<Board>, meta, _args) => {
+        if (meta?.response?.status === 200) {
+          toast.success(`Updated board`, TOAST_OPTIONS);
+        }
+
+        return response;
+      },
+      invalidatesTags: ["Boards", "Columns"],
     }),
+
     deleteBoard: builder.mutation<void, Board>({
       query: (board) => ({
         url: `/boards/${board.id}`,
         method: "DELETE",
       }),
+      transformResponse: (response: void | Promise<void>, meta, _args) => {
+        if (meta?.response?.status === 204) {
+          toast.success(`Successfully deleted board`, TOAST_OPTIONS);
+        }
+
+        return response;
+      },
       invalidatesTags: ["Boards", "Columns", "Tasks"],
     }),
   }),
