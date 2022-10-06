be5b0d06114916ae71818ea7759ec9bcefbc8e28
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-06 11:02:45 +0900

    add toast to columns operations

--- stores/api/columnsApi.tsx
+++ stores/api/columnsApi.tsx
@@ -1,5 +1,7 @@
 import { Board, Column } from "../../lib/types";
 import { apiSlice } from "../apiSlice";
+import { toast } from 'react-toastify';
+import { TOAST_OPTIONS } from '../../constants';
 
 export type AddColumnPayload = {
   boardId: number;
@@ -12,19 +14,35 @@ const columnsApi = apiSlice.injectEndpoints({
       query: (board) => `/boards/${board.id}/columns`,
       providesTags: ["Columns"],
     }),
+
     addColumn: builder.mutation({
       query: (payload: AddColumnPayload) => ({
         url: "/columns",
         method: "POST",
         body: payload,
       }),
+      transformResponse: (response: Column | Promise<Column>, meta, _args) => {
+        if (meta?.response?.status === 201) {
+          toast.success(`Added column`, TOAST_OPTIONS);
+        }
+
+        return response;
+      },
       invalidatesTags: ["Columns"],
     }),
+
     deleteColumn: builder.mutation<void, number>({
       query: (id: number) => ({
         url: `/columns/${id}`,
         method: "DELETE",
       }),
+      transformResponse: (response: void | Promise<void>, meta, _args) => {
+        if (meta?.response?.status === 204) {
+          toast.success(`Deleted column`, TOAST_OPTIONS);
+        }
+
+        return response;
+      },
       invalidatesTags: ["Boards", "Columns"],
     }),
   }),
