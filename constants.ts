cd23b691a23d80ea6762afca2a9ea634ac40ea58
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-06 10:49:18 +0900

    add constants

---
+++ constants.ts
@@ -0,0 +1,12 @@
+import { ToastOptions } from "react-toastify";
+
+export const TOAST_OPTIONS: ToastOptions = {
+  position: "bottom-right",
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: false,
+  draggable: true,
+  progress: undefined,
+  theme: "colored",
+};
