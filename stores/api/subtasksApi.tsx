124c511709e3b5060fd627ba7ff4debdbf053f7c
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-06 11:15:34 +0900

    add toast to subtasks operations

--- stores/api/subtasksApi.tsx
+++ stores/api/subtasksApi.tsx
@@ -1,5 +1,7 @@
 import { Subtask, Task } from "../../lib/types";
 import { apiSlice } from "../apiSlice";
+import { toast } from 'react-toastify';
+import { TOAST_OPTIONS } from '../../constants';
 
 export const subtasksApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -7,27 +9,65 @@ export const subtasksApi = apiSlice.injectEndpoints({
       query: (task: Task) => `/tasks/${task.id}/subtasks`,
       providesTags: ["Subtasks"],
     }),
+
     updateSubtask: builder.mutation<Subtask, Subtask>({
       query: (subtask) => ({
         url: `/subtasks/${subtask.id}`,
         method: "PATCH",
         body: subtask,
       }),
+      transformResponse: async (
+        response: Subtask | Promise<Subtask>,
+        meta,
+        _args
+      ) => {
+        if (meta?.response?.status === 200) {
+          const { title } = await response;
+          toast.success(`Updated subtask '${title}'`, TOAST_OPTIONS);
+        }
+
+        return response;
+      },
       invalidatesTags: ["Tasks", "Subtasks"],
     }),
+
     updateSubtaskCompleted: builder.mutation<Subtask, Subtask>({
       query: (subtask) => ({
         url: `/subtasks/${subtask.id}/completed`,
         method: "PATCH",
         body: subtask,
       }),
-      invalidatesTags: ["Columns", "Tasks", "Subtasks" ]
+      transformResponse: async (
+        response: Subtask | Promise<Subtask>,
+        meta,
+        _args
+      ) => {
+        if (meta?.response?.status === 200) {
+          const { title } = await response;
+          toast.success(`Updated subtask '${title}' status`, TOAST_OPTIONS);
+        }
+
+        return response;
+      },
+      invalidatesTags: ["Columns", "Tasks", "Subtasks"],
     }),
+
     deleteSubtask: builder.mutation<void, Subtask | number>({
       query: (subtask) => ({
         url: `/subtasks/${typeof subtask === "number" ? subtask : subtask.id}`,
         method: "DELETE",
       }),
+      transformResponse: async (
+        response: void | Promise<void>,
+        meta,
+        _args
+      ) => {
+        if (meta?.response?.status === 204) {
+          toast.success(`Deleted subtask`, TOAST_OPTIONS);
+        }
+
+        return response;
+      },
       invalidatesTags: ["Tasks", "Subtasks"],
     }),
   }),
