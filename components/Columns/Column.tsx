f986f7eb37bd597635307e1bef8f56c63b9356d0
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-26 14:47:23 +0900

    add LineWave loader

--- components/Columns/Column.tsx
+++ components/Columns/Column.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "@mui/material";
+import { LineWave } from "react-loader-spinner";
+import { useSelector } from "react-redux";
 import { Column as IColumn } from "../../lib/types";
 import { useTasksQuery } from "../../stores/api/tasksApi";
+import { RootState } from "../../stores/store";
 import TaskCard from "../Tasks/TaskCard";
-import ColumnName from "./ColumnName";
-import { useSelector } from 'react-redux';
-import { RootState } from '../../stores/store';
+import ColumnName from './ColumnName';
+import { PRIMARY_COLOR } from '../../styles/theme';
 
 type Props = {
   column: IColumn;
@@ -13,29 +15,44 @@ type Props = {
 export const COLUMN_WIDTH = "280px";
 
 const Column = ({ column }: Props) => {
-  const accessToken = useSelector((state: RootState) => state.user.accessToken)
-  const { data: tasks, isLoading, error } = useTasksQuery(column, {
-    skip: !accessToken
+  const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const {
+    data: tasks,
+    isLoading,
+    error,
+  } = useTasksQuery(column, {
+    skip: !accessToken,
   });
 
   return (
-    <Stack gap={2} width={COLUMN_WIDTH}>
-      {isLoading && !tasks && <div>Loading...</div>}
-      {tasks && (
-        <>
-          <ColumnName
-            name={column.name.toLowerCase()}
-            color={column.color}
-            tasksNumber={tasks.length}
-          />
-          <Stack gap={3}>
-            {tasks.map((task) => (
-              <TaskCard key={task.id} task={task} column={column}/>
-            ))}
-          </Stack>
-        </>
+    <>
+      {isLoading && !tasks && (
+        <LineWave
+          height="100%"
+          width="100%"
+          color={PRIMARY_COLOR}
+          ariaLabel="line-wave"
+          wrapperStyle={{}}
+          visible={isLoading}
+        />
       )}
-    </Stack>
+      <Stack gap={2} width={COLUMN_WIDTH}>
+        {tasks && (
+          <>
+            <ColumnName
+              name={column.name.toLowerCase()}
+              color={column.color}
+              tasksNumber={tasks.length}
+            />
+            <Stack gap={3}>
+              {tasks.map((task) => (
+                <TaskCard key={task.id} task={task} column={column} />
+              ))}
+            </Stack>
+          </>
+        )}
+      </Stack>
+    </>
   );
 };
 
