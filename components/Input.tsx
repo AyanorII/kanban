7c142a94af7abc9d43725c56ed1a40c48a407d05
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-10-26 14:37:25 +0900

    refactor Input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,18 +1,13 @@
-import { InputLabel, Stack, TextField } from "@mui/material";
+import { InputLabel, Stack, TextField, TextFieldProps } from "@mui/material";
 import { Control, Controller, RegisterOptions } from "react-hook-form";
 import { DANGER_COLOR, WHITE_COLOR } from "../styles/theme";
 
 type Props = {
   name: string;
-  type?: "text" | "password" | "email" | "number" | "date";
-  label: string;
-  error: boolean;
   errorMessage?: string;
   control: Control<any>;
   rules?: RegisterOptions;
-  placeholder?: string;
-  multiline?: boolean;
-};
+} & TextFieldProps;
 
 const Input = ({
   name,
@@ -22,8 +17,7 @@ const Input = ({
   errorMessage,
   control,
   rules,
-  placeholder,
-  multiline,
+  ...props
 }: Props) => {
   return (
     <Controller
@@ -43,8 +37,7 @@ const Input = ({
             type={type || "text"}
             error={error}
             helperText={errorMessage}
-            placeholder={placeholder}
-            multiline={multiline || false}
+            {...props}
           />
         </Stack>
       )}
